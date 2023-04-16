0c1ad0e89fbd40a4a2ed2c178f5975ef2d691c2a
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2023-04-16 15:59:28 -0300

    aula um importação

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('olá mundo');
+import chalk from "chalk";
+import fs from "fs";
+
+function pegaArquivo(caminho) {
+  const encoding = "utf-8";
+  fs.readFile(caminho, encoding, (_, texto) => {
+    console.log(chalk.green(texto));
+  });
+}
+
+pegaArquivo("./arquivos/texto.md");
