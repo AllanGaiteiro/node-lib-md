728751196dba3c9f9a8a8a3f39f910b40ee705f5
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2023-04-16 16:04:39 -0300

    tratamento de erro

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 import chalk from "chalk";
 import fs from "fs";
 
+function trataErro(erro) {
+  throw new Error(chalk.red(erro.code, "não ha arquivo no dietorio"));
+}
+
 function pegaArquivo(caminho) {
   const encoding = "utf-8";
-  fs.readFile(caminho, encoding, (_, texto) => {
+  fs.readFile(caminho, encoding, (erro, texto) => {
+    if (erro) {
+      trataErro(erro);
+    }
     console.log(chalk.green(texto));
   });
 }
